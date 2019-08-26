984d7cade58ac37d3bb15d3f1a94f5e17596c295
joseespg <54192526+joseespg@users.noreply.github.com>
2019-08-26 14:51:46 -0400

    Create login.php

---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Inicio Sesion</title>
+<link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+<body>
+<div class="form">
+<form action="conectar.php" method="POST">
+
+
+<input type="text" name="username" placeholder="Usuario" required >
+<br>
+
+
+<input type ="password" name="password" placeholder="contraseña" required>
+<br>
+    <br>
+    <input type="submit" value="enviar">
+
+</form>
+</div>
+
+</body>
+</html>
